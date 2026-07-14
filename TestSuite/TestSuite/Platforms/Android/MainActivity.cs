ac55cd309e5d711ab1e62de6773e0a18996dc3be
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2026-07-14 21:11:48 +0530

    Appium config implementation

--- TestSuite/TestSuite/Platforms/Android/MainActivity.cs
+++ TestSuite/TestSuite/Platforms/Android/MainActivity.cs
@@ -1,10 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
 
 namespace TestSuite;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Register("com.companyname.testsuite.MainActivity")]
 public class MainActivity : MauiAppCompatActivity
 {
 }
